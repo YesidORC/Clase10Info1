1dd64256f9590ead023d60bb3ca0f75d82e400e3
YesidORC <114194820+YesidORC@users.noreply.github.com>
2024-03-12 17:51:34 -0500

    Agregue archivos de funciones

--- ejemplo.py
+++ ejemplo.py
@@ -1,1 +1,26 @@
-print("Hola  Mundo")
+from reglas import *
+from reglas import solicitar_variables_unicaentre as sv
+import random
+
+while True:
+  menu = input("""
+  1- Suma 34
+  2- resta
+  3- multiplkicación
+  4- división
+  5- Salir 
+  """)
+  x=random.randint(2,49)
+  y=sv()
+  if menu == "1":
+    print(suma(x,y))
+  elif menu == "2":
+    print(resta(x,y))
+  elif menu == "3":
+    print(mult(x,y))
+  elif menu == "4":
+    print(division(x,y))
+  elif menu == "5":
+    break
+  else:
+    print("INGRESE VALOR CORRECTO!!!!")
---
+++ reglas.py
@@ -0,0 +1,10 @@
+def solicitar_variables_unicaentre():
+  return int(input("ingrese un valor"))
+def suma(pepito,pepita):
+  return pepito + pepita
+def resta(pepito,pepita):
+  return pepito - pepita
+def mult(pepito,pepita):
+  return pepito * pepita
+def division(pepito,pepita):
+  return pepito / pepita
